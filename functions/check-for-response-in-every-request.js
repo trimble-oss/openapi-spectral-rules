e6dfb3572c2e229ef819dfecd8c4ed3d8924ce70
sneha_thangavelu@trimble.com <sneha_thangavelu@trimble.com>
2023-08-16 17:44:33 +0530

    Extra fields allow list changes in the path block

--- functions/check-for-response-in-every-request.js
+++ functions/check-for-response-in-every-request.js
@@ -1,9 +1,10 @@
 module.exports = (input) => {
   let keys = Object.keys(input)
-  let http_verbs = keys.filter((verb) => !(['parameters'].includes(verb)));
-  for(let index = 0; index < http_verbs.length; index++)
+  let extraFields = ['parameters', 'servers', 'summary', 'description', '$ref']
+  let httpVerbs = keys.filter((verb) => !(extraFields.includes(verb)));
+  for(let index = 0; index < httpVerbs.length; index++)
   {
-    if (!input[http_verbs[index]]["responses"]) {
+    if (!input[httpVerbs[index]]["responses"]) {
     return [
       {
         message: "Response body missing for the given request.",
